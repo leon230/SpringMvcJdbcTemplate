996fb2796f5e09b27678d6dbe919493716ddc49a
lukasz.homik <homik.lukasz@gmail.com>
2016-10-25 15:08:46 +0200

    Java testing. Working.

---
+++ src/main/java/com/tickets/test/JavaTest.java
@@ -0,0 +1,18 @@
+package com.tickets.test;
+import com.tickets.model.Ticket;
+import junit.framework.Assert;
+/**
+ * Created by Lukasz.Homik on 2016-10-25.
+ */
+public class JavaTest {
+
+    @org.junit.Test
+    public void testTicketCreate(){
+        Ticket ticket = new Ticket();
+        ticket.setAccOwner("AccOwner");
+        Assert.assertNotNull(ticket.getAccOwner());
+        Assert.assertEquals("AccOwner", ticket.getAccOwner());
+
+    }
+
+}
