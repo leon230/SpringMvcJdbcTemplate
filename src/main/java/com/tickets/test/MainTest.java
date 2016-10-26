94b41dfa6503b9bec8b748c968c5e4fe727476fe
Leon <homik.lukasz@gmail.com>
2016-10-26 15:29:11 +0200

    Junit

---
+++ src/main/java/com/tickets/test/MainTest.java
@@ -0,0 +1,35 @@
+package com.tickets.test;
+
+import com.tickets.controller.TicketController;
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+/**
+ * Created by Leon on 2016-10-26.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        TestSuite suite = new TestSuite(JavaTest.class, PermissionTest.class, TicketController.class);
+        TestResult result = new TestResult();
+        suite.run(result);
+        System.out.println("Number of test cases = " + result.runCount());
+/**
+ * Test JavaTest class
+ */
+    Result resultJavaTest = JUnitCore.runClasses(JavaTest.class, TicketControllerTest.class);
+
+    for (Failure failure : resultJavaTest.getFailures()) {
+        System.out.println(failure.toString());
+    }
+
+    System.out.println(resultJavaTest.wasSuccessful());
+/**
+ * Test TicketControllerTest
+ */
+
+
+    }
+}
---
+++ src/main/java/com/tickets/test/PermissionTest.java
@@ -0,0 +1,8 @@
+package com.tickets.test;
+
+/**
+ * Created by Leon on 2016-10-26.
+ */
+public class PermissionTest {
+
+}
