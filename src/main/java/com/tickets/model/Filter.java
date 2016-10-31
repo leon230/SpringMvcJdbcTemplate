aa900791f5d8f4ba95c8a8bb51b29ca117da560a
Leon <homik.lukasz@gmail.com>
2016-10-31 13:25:56 +0100

    Junit

--- src/main/java/com/tickets/model/Filter.java
+++ src/main/java/com/tickets/model/Filter.java
@@ -1,11 +1,5 @@
 package com.tickets.model;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
-
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public class Filter {
